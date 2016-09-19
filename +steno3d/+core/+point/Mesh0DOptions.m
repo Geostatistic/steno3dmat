6f2bd3d76a920c286599cd2d931c8b3c5d3c651d
Franklin Koch <franklin@3ptscience.com>
2016-09-19 17:17:05 -0600

    Initial commit of core resources

---
+++ +steno3d/+core/+point/Mesh0DOptions.m
@@ -0,0 +1,14 @@
+classdef Mesh0DOptions < steno3d.traits.HasTraits
+%MESH0DOPTIONS Summary of this class goes here
+%   Detailed explanation goes here
+
+    properties (Hidden, SetAccess = immutable)
+        M0DOptTraits = {                                                ...
+        }
+    end
+
+    methods
+    end
+
+end
+
---
+++ +steno3d/+core/@CompositeResource/CompositeResource.m
@@ -0,0 +1,12 @@
+classdef CompositeResource < steno3d.core.UserContent
+%COMPOSITERESOURCE Summary of this class goes here
+%   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
