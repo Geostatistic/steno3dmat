5f28ab0a91c50d3186ed8e9b0948d75811a0ded0
fwkoch <franklin@3ptscience.com>
2016-09-30 16:59:22 -0600

    Make upload call fig2steno

--- +steno3d/upload.m
+++ +steno3d/upload.m
@@ -1,7 +1,14 @@
-function upload(fh)
+function upload(fh, public, combine)
 % UPLOAD uploads to steno3d.com
 
-    projs = steno3d.fig2steno(fh);
+    if nargin < 2
+        public = false;
+    end
+    if nargin < 3
+        combine = false;
+    end
+    
+    projs = steno3d.fig2steno(fh, public, combine);
     for i=1:length(projs)
         projs(i).upload()
     end
