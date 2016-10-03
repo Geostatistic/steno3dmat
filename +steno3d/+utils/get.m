1938519dc634320460403175a547f54c9a2bcddf
fwkoch <franklin@3ptscience.com>
2016-10-03 17:15:24 -0600

    get allows query params

--- +steno3d/+utils/get.m
+++ +steno3d/+utils/get.m
@@ -1,4 +1,4 @@
-function resp = get(url)
+function resp = get(url, varargin)
 %GET perform a web request to steno3d.com
 %
 %   RESP = GET(URL) get data from the URL endpoint of steno3d.com, passing
@@ -9,9 +9,9 @@ function resp = get(url)
         error('steno3d:notLoggedIn', 'Please `steno3d.login()`')
     end
     user = steno3d.utils.User.currentUser();
-    resp_string = urlread([user.Endpoint url], 'get',                         ...
-                   {'sshKey', user.ApiKey,                               ...
-                   'client', ['steno3dmat:' steno3d.utils.version()]});
+    resp_string = urlread([user.Endpoint url], 'get',                   ...
+                   [varargin {'sshKey', user.ApiKey,                    ...
+                   'client', ['steno3dmat:' steno3d.utils.version()]}]);
     if nargout == 1
         resp = steno3d.utils.json2struct(resp_string);
     end
