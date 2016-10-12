77abbaa2d02054eedb361829652809795f7fabe4
fwkoch <franklin@3ptscience.com>
2016-10-12 15:31:56 -0600

    Documentation for addData and addImage

--- +steno3d/addData.m
+++ +steno3d/addData.m
@@ -1,8 +1,33 @@
 function addData(varargin)
-%ADDDATA Summary of this function goes here
+%ADDDATA Add a dataset to an existing Steno3d resource
+%   STENO3D.ADDDATA(RESOURCE, DATA) adds matrix or vector DATA to the
+%   Steno3D RESOURCE (Point, Line, Surface, or Volume). This function first
+%   attempts to assign DATA to cell centers then to nodes. If the length of
+%   the data does not match either of these, this function errors.
 %
-%   ADDDATA(res, data)
-%   ADDDATA(res, data, title)
+%   STENO3D.ADDDATA(RESOURCE, TITLE, DATA) adds DATA to the RESOURCE with a
+%   given TITLE string.
+%
+%   Example:
+%       x = [0 1 0 0]; y = [0 0 1 0]; z = [0 0 0 1];
+%       tris = convhull(x, y, z);
+%       [myProject, mySurface] = steno3d.trisurf(tris, x, y, z, 'r');
+%       STENO3D.ADDDATA(mySurface, 'Cell Center Data', rand(4, 1));
+%
+%   If the number of cell centers matches the number of nodes (as is the
+%   case in the example above), this function will default to locating
+%   data on cell centers. However, this can be changed programatically:
+%
+%       mySurface.Data{end}.Location = 'N';
+%
+%   and change the title accordingly:
+%
+%       mySurface.Data{end}.Title = 'Node Data';
+%
+%   See also STENO3D.CORE.DATAARRAY, STENO3D.ADDIMAGE, STENO3D.SCATTER,
+%   STENO3D.LINE, STENO3D.TRISURF, STENO3D.SURFACE, STENO3D.VOLUME
+%
+
 
     narginchk(2, 3);
     
@@ -12,25 +37,26 @@ function addData(varargin)
               'Steno3D composite resources']);
     end
     
-    data = varargin{2};
-    if ~isnumeric(data)
-        error('steno3d:addDataError', 'Data must be numeric');
-    end
-    data = data(:);
-    
     if nargin == 3
-        title = varargin{3};
+        title = varargin{2};
+        data = varargin{3};
         if ~ischar(title)
             error('steno3d:addDataError', 'Data title must be a string');
         end
     else
         title = '';
+        data = varargin{2};
     end
     
-    if length(data) == res.Mesh.nN
-        loc = 'N';
-    elseif length(data) == res.Mesh.nC
+    if ~isnumeric(data)
+        error('steno3d:addDataError', 'Data must be numeric');
+    end
+    data = data(:);
+    
+    if length(data) == res.Mesh.nC
         loc = 'CC';
+    elseif length(data) == res.Mesh.nN
+        loc = 'N';
     else
         error('steno3d:addDataError', ['Data length must equal the '    ...
               'number of nodes (%s) or cell centers(%s)'], res.Mesh.nN, ...
--- +steno3d/addImage.m
+++ +steno3d/addImage.m
@@ -1,10 +1,51 @@
 function addImage(varargin)
-%ADDIMAGE Summary of this function goes here
+%ADDIMAGE Add a PNG image to an existing Steno3d resource
+%   STENO3D.ADDIMAGE(RESOURCE, PNGFILE, WIDTH, HEIGHT) scales PNG image
+%   PNGFILE to size WIDTH x HEIGHT, then projects it in a direction
+%   perpendicular to the horizontal plane onto the Steno3D RESOURCE (Point
+%   or Surface).
 %
-%   ADDIMAGE(res, png, width, height)
-%   ADDIMAGE(res, png, dir1, dim1, dir2, dim2)
-%   ADDIMAGE(..., origin)
-%   ADDIMAGE(..., title)
+%   STENO3D.ADDIMAGE(RESOURCE, PNGFILE, DIR1, DIM1, DIR2, DIM2) scales and
+%   reshapes the PNG image PNGFILE so its x-axis lies along DIR1 with
+%   length DIM1 and its y-axis lies along DIR2 with length DIM2. DIR1 and
+%   DIR2 are either a 1 x 3 vector or 'X', 'Y' or 'Z'. The image is then
+%   projected in a direction perpendicular to DIR1 and DIR2 (the
+%   DIR1-cross-DIR2 direction) onto the Steno3D RESOURCE.
+%
+%   STENO3D.ADDIMAGE(..., ORIGIN) shifts the origin of the image by 1 x 3
+%   vector ORIGIN, prior to projection onto the RESOURCE.
+%
+%   STENO3D.ADDIMAGE(..., TITLE) gives the image a TITLE string. If no
+%   TITLE is provided, the image will be titled based on the PNG file name.
+%
+%   Images can only be added to Points or Surfaces. Also, the images are
+%   simply projected straight onto the Resource. This is different than
+%   MATLAB's 'texturemap' face coloring that wraps the image based on the
+%   underlying surface geometry. To map data to the geometry, use
+%   <a href="matlab: help steno3d.addData">steno3d.addData</a> instead.
+%
+%   Example:
+%       % Generate a png image
+%       [x, y, z] = sphere; surf(x, y, z); h = findobj('Type','surface');
+%       load earth; hemisphere = [ones(257,125),X,ones(257,125)];
+%       set(h,'CData',flipud(hemisphere),'FaceColor','texturemap');
+%       colormap(map); axis equal; view([90 0]);
+%       fig = gcf; fig.Position = [fig.Position(1:3) fig.Position(3)];
+%       ax = gca; ax.Position = [0 0 1 1];
+%       tempFile = [tempname '.png'];
+%       print(fig, '-dpng', tempFile);
+%       % Create a surface
+%       verts = [x(:) y(:) z(:)];
+%       tris = convhull(x(:), y(:), z(:));
+%       [myProject, mySurface] = steno3d.trisurf(tris, verts);
+%       % Add the image
+%       STENO3D.ADDIMAGE(mySurface, tempFile,                           ...
+%                        'X', 2, 'Z', 2, [-1 -1 -1], 'Hemisphere');
+%
+%   See also STENO3D.CORE.TEXTURE2DIMAGE, STENO3D.ADDDATA, STENO3D.SCATTER,
+%   STENO3D.TRISURF, STENO3D.SURFACE
+%
+
 
     narginchk(4, 8);
     
@@ -42,7 +83,8 @@ function addImage(varargin)
         title = varargin{1};
         varargin = varargin(2:end);
     else
-        title = '';
+        titlesplit = strsplit(pngfile, filesep);
+        title = titlesplit{end}(1:end-4);
     end
     
     if ~isempty(varargin)
