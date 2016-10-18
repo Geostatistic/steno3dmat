95e74d9ae4e276d24ad5985427fd5687b5d37449
fwkoch <franklin@3ptscience.com>
2016-10-18 12:12:52 -0600

    volume, adddata, addimage examples

---
+++ +steno3d/+examples/image.m
@@ -0,0 +1,120 @@
+%Steno3D addImage examples
+%
+%   Example 1:
+%       >> % Create a temporary PNG file 
+%       >> pngFile = [tempname '.png'];
+%       >> imwrite(imread('ngc6543a.jpg'), pngFile, 'png');
+%       >> % Create a Project with a Point resource using scatter
+%       >> [example1, pts] = steno3d.scatter(rand(1000, 3));
+%       >> % Add the image to the points, projecting it from the top down
+%       >> steno3d.addImage(pts, pngFile, 1, 1);
+%       >> example1.plot();
+%       >> clear pngFile pts
+%
+%   Example 2:
+%       >> % Create a temporary PNG file 
+%       >> pngFile = [tempname '.png'];
+%       >> imwrite(imread('ngc6543a.jpg'), pngFile, 'png');
+%       >> % Create a Project with a vertical Surface resource
+%       >> [example2, sfc] = steno3d.surface(                          ...
+%              'X', 0:.1:1, 'Z', 0:.1:1, [0 0 0]                        ...
+%          );
+%       >> % Add the image to the points, projecting it horizontally
+%       >> steno3d.addImage(sfc, pngFile, 'X', .25, 'Z', .25);
+%       >> % Add the image to the points with an offset
+%       >> steno3d.addImage(sfc, pngFile, 'X', .25, 'Z', .25,          ...
+%                           [.25 0 .25]);
+%       >> % Add the image to the points with title, offset and reversed
+%       >> steno3d.addImage(sfc, pngFile, [-1 0 0], .5, [0 0 -1], .5,   ...
+%                           [1 0 1], 'Reversed Space Image');
+%       >> example2.plot();
+%       >> clear pngFile sfc
+%
+%   Example 3:
+%       >> % Create a temporary PNG file
+%       >> fig = figure; [x, y, z] = sphere; surf(x, y, z);
+%       >> h = findobj('Type', 'surface');
+%       >> load earth; hemisphere = [ones(257,125),X,ones(257,125)];
+%       >> set(h,'CData',flipud(hemisphere),'FaceColor','texturemap');
+%       >> colormap(map); axis equal; view([90 0]);
+%       >> fig.Position = [fig.Position(1:3) fig.Position(3)];
+%       >> ax = gca; ax.Position = [0 0 1 1];
+%       >> pngFile = [tempname '.png'];
+%       >> print(fig, '-dpng', pngFile);
+%       >> close(fig)
+%       >> clear map X h hemisphere fig ax
+%       >> 
+%       >> % Create a surface
+%       >> verts = [x(:) y(:) z(:)];
+%       >> tris = convhull(x(:), y(:), z(:));
+%       >> [example3, sph] = steno3d.trisurf(tris, verts);
+%       >> % Add the image
+%       >> steno3d.addImage(sph, pngFile, 'X', 2, 'Z', 2, [-1 -1 -1],   ...
+%                           'Hemisphere');
+%       >> example3.plot();
+%       >> clear x y z verts tris sph pngFile
+%
+%   You can <a href="matlab: steno3d.examples.image
+%   ">click here</a> to run the above examples or type:
+%       >> steno3d.examples.image
+%   Then plot the projects with:
+%       >> example1.plot(); % etc...
+%
+%   See also steno3d.addImage, steno3d.core.Texture2DImage,
+%   steno3d.scatter, steno3d.surface, steno3d.trisurf
+%
+
+
+% Example 1:
+% Create a temporary PNG file
+pngFile = [tempname '.png'];
+imwrite(imread('ngc6543a.jpg'), pngFile, 'png');
+% Create a Project with a Point resource using scatter
+[example1, pts] = steno3d.scatter(rand(1000, 3));
+% Add the image to the points, projecting it from the top down
+steno3d.addImage(pts, pngFile, 1, 1);
+example1.plot();
+clear pngFile pts
+
+% Example 2:
+% Create a temporary PNG file
+pngFile = [tempname '.png'];
+imwrite(imread('ngc6543a.jpg'), pngFile, 'png');
+% Create a Project with a vertical Surface resource
+[example2, sfc] = steno3d.surface(                          ...
+    'X', 0:.1:1, 'Z', 0:.1:1, [0 0 0]                        ...
+);
+% Add the image to the points, projecting it horizontally
+steno3d.addImage(sfc, pngFile, 'X', .25, 'Z', .25);
+% Add the image to the points with an offset
+steno3d.addImage(sfc, pngFile, 'X', .25, 'Z', .25,          ...
+                 [.25 0 .25]);
+% Add the image to the points with title, offset and reversed
+steno3d.addImage(sfc, pngFile, [-1 0 0], .5, [0 0 -1], .5,   ...
+                 [1 0 1], 'Reversed Space Image');
+example2.plot();
+clear pngFile sfc
+
+% Example 3:
+% Create a temporary PNG file
+fig = figure; [x, y, z] = sphere; surf(x, y, z);
+h = findobj('Type', 'surface');
+load earth; hemisphere = [ones(257,125),X,ones(257,125)];
+set(h,'CData',flipud(hemisphere),'FaceColor','texturemap');
+colormap(map); axis equal; view([90 0]);
+fig.Position = [fig.Position(1:3) fig.Position(3)];
+ax = gca; ax.Position = [0 0 1 1];
+pngFile = [tempname '.png'];
+print(fig, '-dpng', pngFile);
+close(fig)
+clear map X h hemisphere fig ax
+
+% Create a surface
+verts = [x(:) y(:) z(:)];
+tris = convhull(x(:), y(:), z(:));
+[example3, sph] = steno3d.trisurf(tris, verts);
+% Add the image
+steno3d.addImage(sph, pngFile, 'X', 2, 'Z', 2, [-1 -1 -1],   ...
+                 'Hemisphere');
+example3.plot();
+clear x y z verts tris sph pngFile
---
+++ +steno3d/+examples/volume.m
@@ -0,0 +1,103 @@
+%Steno3D volume plotting examples
+%
+%   Example 1:
+%       >> % Create Steno3D Project with Volume resource from 3D matrix
+%       >> V = flow;
+%       >> example1 = steno3d.volume(V);
+%       >> clear V
+%
+%   Example 2:
+%       >> % Create Project with Volume offset from origin
+%       >> example2 = steno3d.volume([-12.5 5 -12.5], flow);
+%
+%   Example 3:
+%       >> % Create Project with irregularly spaced Volume from a function
+%       >> xedge = [-20:2:-10 -9:9 10:2:20]; yedge = xedge; zedge = -10:10;
+%       >> xcent = (xedge(1:end-1) + xedge(2:end))/2;
+%       >> ycent = (yedge(1:end-1) + yedge(2:end))/2;
+%       >> zcent = (zedge(1:end-1) + zedge(2:end))/2;
+%       >> [X, Y, Z] = ndgrid(xcent, ycent, zcent);
+%       >> dist = sqrt(X.*X + Y.*Y + Z.*Z*4);
+%       >> example3 = steno3d.volume(xcent, ycent, zcent, dist);
+%       >> clear xedge yedge zedge xcent ycent zcent X Y Z dist
+%
+%   Example 4:
+%       >> % Create Project with titled, offset Volume resource
+%       >> % Note: x, y, and z values correspond to spacing here since they
+%       >> % equal the data dimensions
+%       >> example4 = steno3d.volume(                                   ...
+%              2*ones(20, 1), 2*ones(20, 1), 2*ones(10, 1),             ...
+%              [-20 -20 -20], 'Random Volume', rand(20, 20, 10)         ...
+%          );
+%       >> % Add another Volume to that Project
+%       >> [~, myVolume] = steno3d.volume(                              ...
+%              example4, ones(40, 1), ones(40, 1), 2*ones(10, 1),       ...
+%              [-20 -20 0], 'Another Random Volume', rand(40, 40, 10)   ...
+%          );
+%       >> % Modify the Volume directly
+%       >> myVolume.Opts.Opacity = 0.75;
+%       >> myVolume.Mesh.Opts.Wireframe = true;
+%       >> example4.plot();
+%       >> clear myVolume
+%       >> % Note: myVolume is always accessable using:
+%       >> example4.Resources{end};
+%
+%   You can <a href="matlab: steno3d.examples.volume
+%   ">click here</a> to run the above examples or type:
+%       >> steno3d.examples.volume
+%   Then plot the projects with:
+%       >> example1.plot(); % etc...
+%
+% Additional Examples:
+%   <a href="matlab: help steno3d.examples.core.volume
+%   ">volume resources</a>, <a href="matlab:
+%   help steno3d.examples.core.project
+%   ">projects</a>, <a href="matlab: help steno3d.examples.upload
+%   ">uploading</a>, <a href="matlab: help steno3d.examples.plotting
+%   ">other plotting functions</a>
+%
+%   See also steno3d.volume, steno3d.core.Volume, steno3d.core.Project
+%
+
+
+% Example 1:
+% Create Steno3D Project with Volume resource from 3D matrix
+V = flow;
+example1 = steno3d.volume(V);
+clear V
+
+% Example 2:
+% Create Project with Volume offset from origin
+example2 = steno3d.volume([-12.5 5 -12.5], flow);
+
+% Example 3:
+% Create Project with irregularly spaced Volume from a function
+xedge = [-20:2:-10 -9:9 10:2:20]; yedge = xedge; zedge = -10:10;
+xcent = (xedge(1:end-1) + xedge(2:end))/2;
+ycent = (yedge(1:end-1) + yedge(2:end))/2;
+zcent = (zedge(1:end-1) + zedge(2:end))/2;
+[X, Y, Z] = ndgrid(xcent, ycent, zcent);
+dist = sqrt(X.*X + Y.*Y + Z.*Z*4);
+example3 = steno3d.volume(xedge, yedge, zedge, dist);
+clear xedge yedge zedge xcent ycent zcent X Y Z dist
+
+% Example 4:
+% Create Project with titled, offset Volume resource
+% Note: x, y, and z values correspond to spacing here since they
+% equal the data dimensions
+example4 = steno3d.volume(                                   ...
+    2*ones(20, 1), 2*ones(20, 1), 2*ones(10, 1),             ...
+    [-20 -20 -20], 'Random Volume', rand(20, 20, 10)         ...
+);
+% Add another Volume to that Project
+[~, myVolume] = steno3d.volume(                              ...
+    example4, ones(40, 1), ones(40, 1), 2*ones(10, 1),       ...
+    [-20 -20 0], 'Another Random Volume', rand(40, 40, 10)   ...
+);
+% Modify the Volume directly
+myVolume.Opts.Opacity = 0.75;
+myVolume.Mesh.Opts.Wireframe = true;
+example4.plot();
+clear myVolume
+% Note: myVolume is always accessable using:
+example4.Resources{end};
