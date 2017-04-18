5cd1b16d935d3ccac6a467e04f1083571cc032ab
fwkoch <franklin@3ptscience.com>
2017-04-18 10:50:31 -0600

    Only add spacing around images, correct formatting

--- buildFromSrc.m
+++ buildFromSrc.m
@@ -512,8 +512,10 @@ function srcline = sphinxFormat(srcline, srcitem)
     end
 
     %image
-    srcline = strrep(srcline, '%%%image', '.. image::');
-    srcline = ['|\n' srcline '\n|']
+    if strfind(srcline, '%%%image')
+        srcline = strrep(srcline, '%%%image', '.. image::');
+        srcline = ['|\n\n' srcline '\n\n|'];
+    end
 
     srcline = strrep(srcline, '''@', '''\\@');
 
