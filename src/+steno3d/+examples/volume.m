da05905f63f6299aff8a3f5493d703b45923d0ac
poonesh <poonehshooshtari@gmail.com>
2017-04-19 21:23:26 -0600

    Added images and more explanation to the examples

--- src/+steno3d/+examples/volume.m
+++ src/+steno3d/+examples/volume.m
@@ -13,20 +13,26 @@ V = flow;
 example1 = steno3d.volume(V);
 clear V;
 
-%   Example 2: Create Project with Volume resource offset from origin
+%%%image /images/volume-examples-images/volume-example-1.png
+
+%   Example 2: Create Volume resource with an offset of [-12.5 5 -12.5] from origin
 example2 = steno3d.volume([-12.5 5 -12.5], flow);
 
-%   Example 3: Create Project with irregularly spaced Volume resource
+%%%image /images/volume-examples-images/volume-example-2.png
+
+%   Example 3: Create Project with irregularly spaced Volume resource where "dist" data are added to the cell-centers inside the Volume resource
 xedge = [-20:2:-10 -9:9 10:2:20]; yedge = xedge; zedge = -10:10;
 xcent = (xedge(1:end-1) + xedge(2:end))/2;
 ycent = (yedge(1:end-1) + yedge(2:end))/2;
 zcent = (zedge(1:end-1) + zedge(2:end))/2;
 [X, Y, Z] = ndgrid(xcent, ycent, zcent);
 dist = sqrt(X.*X + Y.*Y + Z.*Z*4);
-example3 = steno3d.volume(xcent, ycent, zcent, dist);
+example3 = steno3d.volume(xedge, yedge, zedge, dist);
 clear xedge yedge zedge xcent ycent zcent X Y Z dist;
 
-%   Example 4: Create Project with two Volumes
+%%%image /images/volume-examples-images/volume-example-3.png
+
+%   Example 4: Create Project with a new Volume resource added to it
 [xvals, yvals, zvals] = ndgrid(-8:4:8, -9:2:9, -9.5:9.5);
 example4 = steno3d.volume(                                   ...
     4*ones(5, 1), 2*ones(10, 1), ones(20, 1), [-10 -10 -10], ...
@@ -40,3 +46,5 @@ vol.Opts.Opacity = 0.75;
 vol.Mesh.Opts.Wireframe = true;
 example4.plot();
 clear xvals yvals zvals vol;
+
+%%%image /images/volume-examples-images/volume-example-4.png
