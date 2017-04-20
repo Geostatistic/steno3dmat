2403cd15cc3b33adf67b5ec6befa1461eba44135
poonesh <poonehshooshtari@gmail.com>
2017-04-20 17:14:39 -0600

    Added images to the examples ofaddData

--- src/+steno3d/+examples/adddata.m
+++ src/+steno3d/+examples/adddata.m
@@ -12,14 +12,20 @@
 coords = rand(100, 3);
 [example1, pts] = steno3d.scatter(coords, 'r');
 steno3d.addData(pts, coords(:, 1));
+example1.plot;
 clear coords pts
+
+%%%image /images/addData-examples-images/addData-example-1.png
 
 %   Example 2: Create a %%%ref[Surface](steno3d.core.Surface) and add random face data
 x = [0 1 0 0]; y = [0 0 1 0]; z = [0 0 0 1];
 tris = convhull(x, y, z);
 example2 = steno3d.trisurf(tris, x, y, z, 'r');
 steno3d.addData(example2.Resources{1}, 'Face Data', rand(4, 1));
+example2.plot;
 clear x y z tris
+
+%%%image /images/addData-examples-images/addData-example-2.png
 
 %   Example 3: Create a grid Surface and add four datasets
 %
@@ -27,8 +33,12 @@ clear x y z tris
 %       pks is a grid with 20x20 edges and 19x19 faces. Data of
 %       several different sizes can be added
 [example3, pks] = steno3d.surface(peaks(20));
+example3.plot;
 steno3d.addData(pks, 'Node Data from Matrix', rand(20, 20));
 steno3d.addData(pks, 'Node Data from Array', rand(20*20, 1));
 steno3d.addData(pks, 'Face Data from Matrix', rand(19, 19));
 steno3d.addData(pks, 'Face Data from Array', rand(19*19, 1));
+example3.plot;
 clear pks
+
+%%%image /images/addData-examples-images/addData-example-3.png
