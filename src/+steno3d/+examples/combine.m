f923b35b9351d54bf5a17d3a7e6b3e5a9affb003
poonesh <poonehshooshtari@gmail.com>
2017-04-23 23:27:36 -0600

    Added image for Combine page example

--- src/+steno3d/+examples/combine.m
+++ src/+steno3d/+examples/combine.m
@@ -10,17 +10,24 @@
 
 %   Example 1: Combine three Steno3D %%%ref[Projects](steno3d.core.Project)
 pointProj = steno3d.scatter(rand(100, 3)); close;
-lineProj = steno3d.line(-10:.1:0, sin(-10:.1:0), cos(-10:.1:0));
-volProj = steno3d.volume([1 1 1], rand(5, 7, 10)); close; close;
+lineProj = steno3d.line(-10:.1:0, sin(-10:.1:0), cos(-10:.1:0)); close;
+volProj = steno3d.volume([1 1 1], rand(5, 7, 10)); close;
 example1 = steno3d.combine(pointProj, lineProj, volProj);
+example1.plot;
 clear pointProj lineProj volProj;
+
+%%%image /images/combine-examples-images/combine-example-1.png
+
 
 %   Example 2: Combine two Projects converted from MATLAB figures
 figure; peaks; peaksProj = steno3d.convert(gca); close;
 figure; sphere; sphereProj = steno3d.convert(gca); close;
 example2 = steno3d.combine(peaksProj, sphereProj);
+example2.plot;
 example2.Title = 'Two-Surface Project';
 clear peaksProj sphereProj;
+
+%%%image /images/combine-examples-images/combine-example-2.png
 
 %   Example 3: Combine four Projects converted from a multi-axes figure
 fig = figure;
@@ -30,4 +37,7 @@ subplot(223); [x, y] = ndgrid(.2:.1:3, .2:.1:3); peaks(x, y);
 subplot(224); [x, y] = ndgrid(.2:.1:3, -3:.1:0); peaks(x, y);
 projs = steno3d.convert(fig);
 example3 = steno3d.combine(projs);
+example3.plot;
 close; clear fig x y projs;
+
+%%%image /images/combine-examples-images/combine-example-3.png
