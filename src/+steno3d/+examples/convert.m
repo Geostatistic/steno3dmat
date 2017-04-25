e55a69c739bdb6b5d10fb60e42bc3221db9ae937
poonesh <poonehshooshtari@gmail.com>
2017-04-25 17:46:49 -0600

    Added images for convert examples

--- src/+steno3d/+examples/convert.m
+++ src/+steno3d/+examples/convert.m
@@ -11,12 +11,18 @@
 %   Example 1: Convert MATLAB axes to Steno3D %%%ref[Project](steno3d.core.Project)
 figure; peaks;
 example1 = steno3d.convert(gca);
+example1.plot;
 close;
+
+%%%image /images/convert-examples-images/convert-example-1.png
 
 %   Example 2: Convert figure with multiple graphics to Steno3D
 figure; peaks; hold on; sphere; cylinder
 example2 = steno3d.convert(gcf);
+example2.plot;
 close;
+
+%%%image /images/convert-examples-images/convert-example-2.png
 
 %   Example 3: Combine four axes into one project with multiple resources
 fig = figure;
@@ -25,7 +31,10 @@ subplot(222); [x, y] = ndgrid(-3:.1:0, .2:.1:3); peaks(x, y);
 subplot(223); [x, y] = ndgrid(.2:.1:3, .2:.1:3); peaks(x, y);
 subplot(224); [x, y] = ndgrid(.2:.1:3, -3:.1:0); peaks(x, y);
 example3 = steno3d.convert(fig, 'CombineAxes', true);
+example3.plot;
 close; clear fig x y;
+
+%%%image /images/convert-examples-images/convert-example-3.png
 
 %   Example 4: Combine four axes and combine all their graphics
 fig = figure;
@@ -35,4 +44,7 @@ subplot(223); [x, y] = ndgrid(.2:.1:3, .2:.1:3); peaks(x, y);
 subplot(224); [x, y] = ndgrid(.2:.1:3, -3:.1:0); peaks(x, y);
 example4 = steno3d.convert(fig, 'CombineAxes', true,         ...
                            'CombineResources', true);
-close; clear fig x y; example4.plot();
+example4.plot();
+close; clear fig x y;
+
+%%%image /images/convert-examples-images/convert-example-4.png
